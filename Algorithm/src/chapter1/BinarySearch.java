b009836876d3b24f7b1be7ccbcbd72d9737a9756
sinno.song <sinno.song@outlook.com>
2022-08-28 15:36:38 +0800

    delete cSharp project

---
+++ Algorithm/src/chapter1/BinarySearch.java
@@ -0,0 +1,39 @@
+package chapter1;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BinarySearch {
+    public static int rank(int key, int[] a) {
+        // 数组必须是有序的
+        int low = 0;
+        int high = a.length - 1;
+        while (low < high) {
+            // 被查找的键要么不存在，要么存在于a[lo.hi]之中
+            int mid = low + (high - low) / 2;
+            if (key < a[mid]) {
+                high = mid - 1;
+            } else if (key > a[mid]) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] whiteList = In.readInts(args[0]);
+        Arrays.sort(whiteList);
+        while (!StdIn.isEmpty()) {
+            // 读取键值，如果不存在于白名单中则将其打印
+            int key = StdIn.readInt();
+            if (rank(key, whiteList) < 0) {
+                StdOut.println(key);
+            }
+        }
+    }
+}
--- Algorithm/src/chapter1/Gcd.java
+++ Algorithm/src/chapter1/Gcd.java
@@ -1,4 +1,4 @@
-package com.sinno.chapter1;
+package chapter1;
 
 public class Gcd {
     public static int gcd(int p, int q) {
